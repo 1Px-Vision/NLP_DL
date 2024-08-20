dad8ca1daa7fb0c92b2fc6e551022ebe9b84b60b
1Px-Vision <150855410+1Px-Vision@users.noreply.github.com>
2024-08-20 15:27:33 -0600

    project_tests.py

--- Project_2_Machine_Translation/project_tests.py
+++ Project_2_Machine_Translation/project_tests.py
@@ -7,7 +7,7 @@ from keras.utils import to_categorical
 
 def _test_model(model, input_shape, output_sequence_length, french_vocab_size):
     if isinstance(model, Sequential):
-        model = model.model
+        model = model#.model
 
     assert model.input_shape == (None, *input_shape[1:]),\
         'Wrong input shape. Found input shape {} using parameter input_shape={}'.format(model.input_shape, input_shape)
